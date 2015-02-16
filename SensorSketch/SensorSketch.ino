e77b5e46e6e209bfcb08c4a39c495f098fdc3533
stepjue <stepjue@gmail.com>
2015-02-15 17:30:40 -0800

    Fixed BLE name (must be 10 characters or less). Removed local variables. Using ble_write_bytes(). Header added to data to signify whether it is red or infrared readings

--- SensorSketch/SensorSketch.ino
+++ SensorSketch/SensorSketch.ino
@@ -11,63 +11,62 @@
 #include <SPI.h>
 #include <boards.h>
 #include <RBL_nRF8001.h>
-#include <services.h> 
+#include <RBL_services.h>
 
-//Constants
+// BLE shield contraints
+#define MAX_TX_BUFF 64  // For Tinyshield, 20
+#define MAX_RX_BUFF 64  // For Tinyshield, 20
+#define BLE_BAUD_RATE 57600
+
+// Constants
 const int infraredPin = 3;
-const int redLEDPin = 2; 
+const int redPin = 2; 
 const int sensorPin = A0;
 const int boardLED = 13;
-
-//Variables
-const int binSize = 100;
-uint8_t dataBin[binSize]; //Size should correspond with packet sent via BLE
-int i = 0;
+const int binSize = MAX_TX_BUFF;
 
 void setup() {
-  
   //BLE Setup
-  ble_set_name("PulseFinder");
   ble_begin();
-  
+  ble_set_name("Pulse"); // Name cannot be longer than 10 characters
+  ble_do_events(); // Set initial status of board
+
   // Enable serial debug
-  Serial.begin(57600);
+  Serial.begin(BLE_BAUD_RATE);
 
   //Sensor Setup
   pinMode(boardLED, OUTPUT);
-
   pinMode(infraredPin, OUTPUT);
-  pinMode(redLEDPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
 }
 
 void loop() {
-  uint8_t sensorValue = analogRead(sensorPin);
-  dataBin[i++] = sensorValue;
-//  Serial.println(sensorValue);
-  
-  digitalWrite(boardLED, LOW);
+  uint8_t dataBin[binSize], sensorValue;
+  int isRed = ble_read();
+  togglePin(isRed);
+  Serial.println(isRed);
 
-  if (i == binSize-1) {
-      digitalWrite(boardLED, HIGH);
-      
-      for(int j=0; j < binSize; j++) {
-          ble_write(dataBin[j]);
-      }  
-      
-      i = 0; //Reset Bin
-  }
-  else { //BLE not connected
-    digitalWrite(boardLED, LOW);
+  //Serial.println();   // Debugging
+  for(int i = 0; i < binSize; i++) {
+    dataBin[i] = analogRead(sensorPin);
+    //Serial.println(dataBin[i]);   // Debugging
   }
 
-  //Crucial
+  ble_write_bytes((unsigned char *)dataBin, (unsigned char)binSize);
+
+  // Updates status of the BLE connection (connected, available, busy, etc.)
+  // Transmits and recieves data using buffer arrays built from ble_write, ble_read, etc.
   ble_do_events();
-  
-  if ( ble_available() ) {
-//      Serial.print("Reading: ");    
-//      Serial.println(ble_read());
+}
+
+// isRed = 1 -> turn on red pin, turn off ir pin
+// isRed = 0 -> turn on ir pin, turn off red pin
+void togglePin(int isRed) {
+  if(isRed) {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(infraredPin, LOW);
+  } else {
+    digitalWrite(infraredPin, HIGH);
+    digitalWrite(redPin, LOW);
   }
-  
-  digitalWrite(infraredPin, HIGH);
-  digitalWrite(redLEDPin, LOW); 
 }
