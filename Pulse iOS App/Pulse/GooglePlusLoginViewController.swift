25df173ea52a2bbc1f7b89c1439f7ccc36ece7e9
SLewson <slewson@calpoly.edu>
2015-03-16 18:15:40 -0700

    logs in

--- Pulse iOS App/Pulse/GooglePlusLoginViewController.swift
+++ Pulse iOS App/Pulse/GooglePlusLoginViewController.swift
@@ -18,7 +18,7 @@ class GooglePlusLoginViewController: UIViewController, GPPSignInDelegate {
         var signIn = GPPSignIn.sharedInstance()
         signIn.shouldFetchGooglePlusUser = true
         signIn.clientID = kClientId
-        signIn.scopes = [kGTLAuthScopePlusLogin]
+        signIn.scopes = [kGTLAuthScopePlusUserinfoProfile]
         signIn.delegate = self
         // Do any additional setup after loading the view.
     }
@@ -32,26 +32,6 @@ class GooglePlusLoginViewController: UIViewController, GPPSignInDelegate {
     func finishedWithAuth(auth: GTMOAuth2Authentication!, error: NSError!) {
         println("FinishedWithAuth: \(auth) and error: \(error)")
     }
-    
-    /*
-- (void)viewDidLoad {
-[super viewDidLoad];
-
-GPPSignIn *signIn = [GPPSignIn sharedInstance];
-signIn.shouldFetchGooglePlusUser = YES;
-//signIn.shouldFetchGoogleUserEmail = YES;  // Uncomment to get the user's email
-
-// You previously set kClientId in the "Initialize the Google+ client" step
-signIn.clientID = kClientId;
-
-// Uncomment one of these two statements for the scope you chose in the previous step
-signIn.scopes = @[ kGTLAuthScopePlusLogin ];  // "https://www.googleapis.com/auth/plus.login" scope
-//signIn.scopes = @[ @"profile" ];            // "profile" scope
-
-// Optional: declare signIn.actions, see "app activities"
-signIn.delegate = self;
-}
-*/
 
     /*
     // MARK: - Navigation
